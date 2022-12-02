18c6292e8ef197d42543c9b1ad7723fbe6510ada
Daniel Roe <daniel@roe.dev>
2022-12-02 23:26:17 +0000

    perf: improve split string performance

--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,23 @@ export function capitalise(text: string) {
   return text[0].toUpperCase() + text.slice(1)
 }
 
+const UC_RE = /[A-Z]/
 export function splitStringByCase(text: string) {
-  return capitalise(
-    text
-      .replace(/[A-Z]+[a-z]/g, h =>
-        h.length > 2
-          ? h.slice(0, -2) + ' ' + capitalise(h.slice(-2).toLowerCase())
-          : Array(h).join(' ')
-      )
-      .replace(/[a-z][A-Z]+/g, h => `${h[0]} ${h.slice(1)}`)
-  )
+  let output = ''
+  let index = 0
+  for (const char of text) {
+    const lastChar = index ? text[index - 1] : ''
+    const nextChar = text[index + 1]
+    if (
+      UC_RE.test(char) &&
+      (!UC_RE.test(lastChar) || (nextChar && !UC_RE.test(nextChar)))
+    ) {
+      output += ' '
+    }
+    output += char
+    index++
+  }
+  return capitalise(output)
 }
 
 export function inArray<A>(items: A | A[]) {
